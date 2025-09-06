91a39c25dca9242ebad2f9078991e1a67f6c7f93
JjPujante-dev <joshuajen166@gmail.com>
2025-09-06 18:20:15 +0800

    add main.cpp that reads a csv file and print each line to the terminal.

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+// Function to split a CSV line by a delimeter
+std::vector<std::string> splitString(const std::string line, char delimiter
+) {
+    std::vector<std::string> tokens;
+    std::stringstream ss(line);
+    std::string token;
+
+    while (std::getline(ss, token, delimiter )) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+int main()
+{
+    std::ifstream file("transactions.csv"); //open CSV file
+    if (!file.is_open()) {
+         std::cout << "Error: Could not open file!" << std::endl;
+         return 1;
+    }
+
+    std::string line;
+    while ( std::getline(file, line)) {
+        std::vector<std::string> values = splitString(line, ',');
+
+        // Print each value
+        for (size_t i = 0; i < values.size(); i++) {
+            std::cout << "" << values[i] << "";
+            if (i < values.size() - 1) std::cout << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    file.close();
+    return 0;
+}
